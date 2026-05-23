25780a9a0e0af551ceeb0ee85749b547703d0687
csmccreery <cameron@mccreery.dev>
2026-05-22 20:50:59 -0400

    Shuffling files around to make this a bit easier so maintain in the future

--- pyrite/__main__.py
+++ pyrite/__main__.py
@@ -1,17 +1,23 @@
-from tokenizer import *
+from .tokenizer import tokenize
 from pathlib import Path
 
 
-def main() -> None:
-    file_location = Path(__file__).resolve()
+def parse_md(path):
+    corrected_path = Path(path)
+    with open(corrected_path, "r") as f:
+        file_content = f.readlines()
+        print(file_content)
 
-    with open(file_location.parent.parent / "tests" / "test.md", 'r') as fin:
-        lines = fin.readlines()
-        root = tokenize(lines)
-        for child in root.children:
-            print(child)
+    root = tokenize(file_content)
+    return root
 
-    print(file_location)
+
+def main():
+    current_dir = Path(__file__).parent.resolve()
+    root = parse_md(current_dir / "tests" / "test.md")
+
+    for child in root.children:
+        print(child)
 
 
 if __name__ == "__main__":
